395701234a5baceb115acd6a5818521395a01efd
楼教主 <cik520@qq.com>
2016-04-05 10:28:20 +0800

    优化正则

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-hexo.extend.filter.register('after_post_render', function(data){
-  data.content = data.content.replace(/<img src="([^"]+@(\dx)\.[^"]+)"/g, '$& srcset="$1 $2"');
+hexo.extend.filter.register('after_post_render', function(data) {
+  data.content = data.content.replace(/<img src="([^"]+@(\dx)[^"]*)"/g, '$& srcset="$1 $2"');
 
   return data;
 });
